e2786296e3ab74fa722d108c45e15436d0dc75b6
bhuvanesh-kp <bhuvaneshkpb@gmail.com>
2023-12-20 11:03:27 +0530

    Model training

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,7 @@ import sys
 from src.exception import CustomException
 from src.logger import logging
 from src.components.data_transformation import *
+from src.components.model_trainer import *
 
 @dataclass
 class DataIngestionConfig():
@@ -47,4 +48,8 @@ if __name__ == '__main__':
     train_set,test_set = obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_set,test_set)
+    train_arr,test_arr,_= data_transformation.initiate_data_transformation(train_set,test_set)
+
+    model_trainer = ModelTrainer()
+    r2_square = model_trainer.initiate_model_trainer(train_arr,test_arr)
+    print(r2_square)
